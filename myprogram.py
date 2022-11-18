9f6de663e3a43099495540be13e5e312c7d267d4
Ellie Hecha <loverlyflowerly@gmail.com>
2022-11-18 17:51:29 -0500

    Added sorting feautre

--- myprogram.py
+++ myprogram.py
@@ -1,9 +1,23 @@
+#pip install termcolor
+from termcolor import colored
+# define  punctuation
+punctuations = '''!()-[]{};:'",<>./?@#$%^&*_~'''
+
+
+
 #Program to sort alphabetically the words froma a string provied by the user
-my_str = input ('Enter a string: ')
+my_str = 'hello, world. how are you?'
+
+
+#remove punctuation
+no_punct = ""
+for char in my_str:
+    if char not in punctuations:
+        no_punct = no_punct + char
 
 #breakdown the string int a lsit of words
 
-words = (word.lower() for word in my_str.split())
+words = (word.lower() for word in no_punct.split())
 
 #sort the list
 
@@ -13,4 +27,4 @@ words.sort()
 print("the sorted words are: ")
 
 for word in words:
-    print(word)
+    print(colored(word, 'blue'))
